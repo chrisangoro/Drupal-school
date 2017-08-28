aa49b96fe6f6b6f975bd97109584fad823b07f3e
Christian Gonzalez <chrisangoro@gmail.com>
2017-08-28 15:33:47 -0500

    Addded the shadow-ey overlay to the slider, fixed the js tabs (it's working now) and fixed the positioning of the tabs

--- themes/racc/src/js/tabs.js
+++ themes/racc/src/js/tabs.js
@@ -1,9 +1,13 @@
 //function menu1(){
   //  document.getElementById("dropdown-menu-1").classList.toggle("show");
 //}
-(function ($, Drupal){
-var main = function(){
-        //change tab info interactions
+
+(function ($, Drupal) {
+    Drupal.behaviors.myModuleBehavior = {
+      attach: function (context, settings) {
+
+        console.log('holi');
+        //tab info interactions
         $('.views-row:first-child .views-field-info').click(function(){
             $('.views-row:first-child .views-field-field-nested-paragraph').show();
             $('.views-row:nth-child(2) .views-field-field-nested-paragraph').hide(false);
@@ -24,7 +28,7 @@ var main = function(){
     
             $('.views-row:nth-child(2) .views-field-info').css("background-color", "#f23522");
             $('.views-row:first-child .views-field-info').css("background-color", "#87888e");
-            $('#.views-row:nth-child(3) .views-field-info').css("background-color", "#bbbbbf");
+            $('.views-row:nth-child(3) .views-field-info').css("background-color", "#bbbbbf");
             $('.views-row:nth-child(4) .views-field-info').css("background-color", "#cfcfd2");
         })
     
@@ -51,18 +55,14 @@ var main = function(){
             $('.views-row:nth-child(2) .views-field-info').css("background-color", "#999ba1");
             $('.views-row:nth-child(3) .views-field-info').css("background-color", "#bbbbbf");
         })
-    
-    
+
+      }
+    };
+  })(jQuery, Drupal);
+
     //red filter on tabs images
         // if ($('#row1').css('backgroun-color') == "#f23522") {
         //     $('#row1').hover(function(){
         //         $('#filter').show();
         //     })
         // }
-    
-   
-    }
-    
-    $(document).ready(main);
-
-})(jQuery, Drupal);
